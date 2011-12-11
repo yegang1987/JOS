076ec455e73bd67701fdf62e99027e413e95d0ed
叶刚 <gang.ye1987@gmail.com>
2011-12-11 19:15:53 +0800

    implement memory managerment:page man&page map of jos

---
+++ kern/init.c
@@ -0,0 +1,86 @@
+/* See COPYRIGHT for copyright information. */
+
+#include <inc/stdio.h>
+#include <inc/string.h>
+#include <inc/assert.h>
+
+#include <kern/monitor.h>
+#include <kern/console.h>
+#include <kern/pmap.h>
+#include <kern/kclock.h>
+#include <kern/trap.h>
+
+
+void
+i386_init(void)
+{
+	extern char edata[], end[];
+	uint32_t *ctorva;
+
+	// Initialize the console.
+	// Can't call cprintf until after we do this!
+	cons_init();
+
+	cprintf("6828 decimal is %o octal!\n", 6828);
+
+	// Lab 2 memory management initialization functions
+	boot_mem_init();
+	page_init();
+	page_check();
+
+	// Lab 2 interrupt and gate descriptor initialization functions
+	idt_init();
+
+	// Test IDT (lab 2 only)
+	__asm__ __volatile__("int3");
+	cprintf("Breakpoint succeeded!\n");
+
+	// Drop into the kernel monitor.
+	while (1)
+		monitor(NULL);
+}
+
+
+/*
+ * Variable panicstr contains argument to first call to panic; used as flag
+ * to indicate that the kernel has already called panic.
+ */
+static const char *panicstr;
+
+/*
+ * Panic is called on unresolvable fatal errors.
+ * It prints "panic: mesg", and then enters the kernel monitor.
+ */
+void
+_panic(const char *file, int line, const char *fmt,...)
+{
+	va_list ap;
+
+	if (panicstr)
+		goto dead;
+	panicstr = fmt;
+
+	va_start(ap, fmt);
+	cprintf("kernel panic at %s:%d: ", file, line);
+	vcprintf(fmt, ap);
+	cprintf("\n");
+	va_end(ap);
+
+dead:
+	/* break into the kernel monitor */
+	while (1)
+		monitor(NULL);
+}
+
+/* like panic, but don't */
+void
+_warn(const char *file, int line, const char *fmt,...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	cprintf("kernel warning at %s:%d: ", file, line);
+	vcprintf(fmt, ap);
+	cprintf("\n");
+	va_end(ap);
+}
